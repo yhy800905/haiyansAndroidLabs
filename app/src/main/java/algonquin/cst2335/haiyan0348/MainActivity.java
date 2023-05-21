8b19ef6b013ae523fbb3dfffb3eb3d8eaa36e531
Haiyan Yang <yhy800905@gmail.com>
2023-05-21 13:24:43 -0400

    showed text widgets

--- app/src/main/java/algonquin/cst2335/haiyan0348/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/haiyan0348/MainActivity.java
@@ -3,6 +3,11 @@ package algonquin.cst2335.haiyan0348;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myText = findViewById(R.id.textview);
+
+        Button btn = findViewById(R.id.mybutton);
+
+        EditText myEdit = findViewById(R.id.myedittext);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String words = myEdit.getText().toString();
+
+                myText.setText(words);
+            }
+        });
+
+
     }
 }
