acf8a9bbb624c8725245836d804f4a075220b233
Haiyan Yang <yhy800905@gmail.com>
2023-05-22 19:19:20 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/haiyan0348/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/haiyan0348/ui/MainActivity.java
@@ -1,28 +1,15 @@
 package algonquin.cst2335.haiyan0348.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-import android.os.VibrationAttributes;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import algonquin.cst2335.haiyan0348.R;
 import algonquin.cst2335.haiyan0348.data.MainViewModel;
 import algonquin.cst2335.haiyan0348.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel model;
     private ActivityMainBinding variableBinding;
-    private boolean selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(variableBinding.getRoot());
 
         model.editString.observe(this, s ->
-                variableBinding.textview.setText("Your edit text has" + s));
+                variableBinding.textview.setText("Your edit text has: " + s));
 
         variableBinding.myButton.setOnClickListener(click ->
                 model.editString.postValue(variableBinding.myEdittext.getText().toString()));
@@ -56,10 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
         variableBinding.mySwitch.setOnCheckedChangeListener((btn, isChecked) -> {
             model.isSelected.postValue(isChecked);
-        });
-
-        variableBinding.myImageView.setOnClickListener(click -> {
-            variableBinding.textview.setText("You clicked the picture");
         });
 
         variableBinding.myImageButton.setOnClickListener(click -> {
