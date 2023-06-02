782f1f6bcb5dac4f7fa76d1975883b6479db779d
Haiyan Yang <yhy800905@gmail.com>
2023-06-02 16:16:09 -0400

    Lab4 SecondActivity

--- app/src/main/java/algonquin/cst2335/haiyan0348/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/haiyan0348/MainActivity.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.haiyan0348;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
