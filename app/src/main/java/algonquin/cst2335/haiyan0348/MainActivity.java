b46ee53aff67eb35faed7fa8638e54da3e83158e
Haiyan Yang <yhy800905@gmail.com>
2023-06-09 17:51:18 -0400

    Week4

--- app/src/main/java/algonquin/cst2335/haiyan0348/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/haiyan0348/MainActivity.java
@@ -2,40 +2,98 @@ package algonquin.cst2335.haiyan0348;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.File;
+import java.text.BreakIterator;
+
+import algonquin.cst2335.haiyan0348.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Log.e(TAG, "In onCreate() - Loading Widgets");
+
+        //where can you save files:
+        File myDir =getFilesDir();
+        String path = myDir.getAbsolutePath();
+
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        binding.emailText.setText(emailAddress);
+
+        binding.loginButton.setOnClickListener(clk -> {
+            Log.e(TAG, "You clicked the button");
+
+            String whatIsTyped = binding.emailText.getText().toString();
+
+            //get an editor
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName", whatIsTyped);
+            editor.putFloat("Hi", 4.5f);
+            editor.putInt("Age", 35);
+            editor.apply();
+
+            //saves to disk
+            editor.commit();
+
+            // Retrieve the saved text from SharedPreferences and update the EditText
+            // binding.emailText.setText(prefs.getString("LoginName", " "));
+
+            //where to go:             leaving here          going to SecondActivity
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            nextPage.putExtra( "EmailAddress",binding.emailText.getText().toString() );
+
+//            Intent intent = new Intent(Intent.ACTION_SEND);
+//            intent .setType("*/*");
+//            intent .putExtra(Intent.EXTRA_EMAIL, new String[]{"A@A.com", "B@B.com"}  );
+//            intent .putExtra(Intent.EXTRA_SUBJECT, "Subject");
+
+            //go to another page, carries all the data to the next page
+            startActivity(nextPage);
+
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-    }
+        Log.e(TAG, "The onStart()- the application is now visible on the screen");
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d( TAG, "The onResume()- the application is now responding to user input" );
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e( TAG, "onPause()- The application no longer responds to user input" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( TAG, "he application is no longer visible" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e( TAG, "Any memory used by the application is freed" );
+    }
+
 }
--- app/src/main/java/algonquin/cst2335/haiyan0348/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/haiyan0348/SecondActivity.java
@@ -1,14 +1,108 @@
 package algonquin.cst2335.haiyan0348;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import algonquin.cst2335.haiyan0348.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
+    private ActivitySecondBinding binding;
+    private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(binding.getRoot());
+
+        Intent fromPrevious = getIntent();
+        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
+        String day = fromPrevious.getStringExtra("DAY");
+        int age = fromPrevious.getIntExtra("AGE", 0);
+        int something = fromPrevious.getIntExtra("SOMETHING", 0);//default is for when SOMETHING is not there
+
+        binding.textView3.setText("Welcome back " + emailAddress);
+
+
+        binding.callNumberButton.setOnClickListener((v) -> {
+            String phoneNumber = binding.editTextPhone.getText().toString(); // Get the phone number from the EditText or any other source
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(callIntent);
+        });
+
+        File file = new File( getFilesDir(), "Picture.png");
+        if(file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+            binding.imageView.setImageBitmap(theImage);
+             }
+
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            binding.imageView.setImageBitmap(thumbnail);
+                            FileOutputStream fOut = null;
+                            try {
+                                fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                                thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                fOut.flush();
+                                fOut.close();
+                            }
+                            catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                            catch(IOException ioe) {
+                                ioe.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        );
+
+//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        binding.changePictureButton.setOnClickListener( click -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                    cameraResult.launch(cameraIntent);
+                else
+                    requestPermissions(new String[] {Manifest.permission.CAMERA}, 20);
+            }
+    });
+
+        binding.goBackButton.setOnClickListener((v) -> {
+            //opposite of startActivity()
+            finish(); //go back to previous
+        });
+
     }
 }
+
